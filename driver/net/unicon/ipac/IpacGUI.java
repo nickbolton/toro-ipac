f08ce5e59b0eae57b8889602b395599051ee649c
bolton.nick <bolton.nick@cfe4ea3d-4a2c-0410-b1af-bfcde40e8619>
2007-03-22 22:39:28 +0000

    changed to open source file headers

--- driver/net/unicon/ipac/IpacGUI.java
+++ driver/net/unicon/ipac/IpacGUI.java
@@ -1,15 +1,26 @@
 /*
- *******************************************************************************
+ * Copyright (C) 2007 Unicon, Inc.
  *
- * File:       IpacGUI.java
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * Copyright:  ©2003 Unicon, Inc. All Rights Reserved
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * This source code is the confidential and proprietary information of Unicon.
- * No part of this work may be modified or used without the prior written
- * consent of Unicon.
+ * You should have received a copy of the GNU General Public License
+ * along with this distribution.  It is also available here:
+ * http://www.fsf.org/licensing/licenses/gpl.html
  *
- *******************************************************************************
+ * As a special exception to the terms and conditions of version 
+ * 2 of the GPL, you may redistribute this Program in connection 
+ * with Free/Libre and Open Source Software ("FLOSS") applications 
+ * as described in the GPL FLOSS exception.  You should have received
+ * a copy of the text describing the FLOSS exception along with this
+ * distribution.
  */
 package net.unicon.ipac;
 
@@ -57,7 +68,7 @@ public class IpacGUI extends JFrame implements ActionListener {
     private int windowHeight = 700;
     private int columnWidth  = 40;
 
-    private static final String APP_PATH = "/ipac";
+    private static final String APP_PATH = "/ipac/ipac";
 
     public IpacGUI() throws Exception {
         this.setTitle("IPAC GUI");
--- source/net/unicon/ipac/IPACResult.java
+++ source/net/unicon/ipac/IPACResult.java
@@ -1,15 +1,26 @@
 /*
- *******************************************************************************
+ * Copyright (C) 2007 Unicon, Inc.
  *
- * File:       IPACResult.java
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * Copyright:  ©2003 Unicon, Inc. All Rights Reserved
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * This source code is the confidential and proprietary information of Unicon.
- * No part of this work may be modified or used without the prior written
- * consent of Unicon.
+ * You should have received a copy of the GNU General Public License
+ * along with this distribution.  It is also available here:
+ * http://www.fsf.org/licensing/licenses/gpl.html
  *
- *******************************************************************************
+ * As a special exception to the terms and conditions of version 
+ * 2 of the GPL, you may redistribute this Program in connection 
+ * with Free/Libre and Open Source Software ("FLOSS") applications 
+ * as described in the GPL FLOSS exception.  You should have received
+ * a copy of the text describing the FLOSS exception along with this
+ * distribution.
  */
 
 package net.unicon.ipac;
